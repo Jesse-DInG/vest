a7483d30b8877b70a54df9f5481fe3357cf4baff
Jesse-Ding <dingxj7788@gmail.com>
2017-08-10 16:17:38 +0800

    update

---
+++ src/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app">
+        <SuiteListView>
+        </SuiteListView>
+        <div>
+            <div class="left-view">
+            <CaseListView></CaseListView>
+            </div>
+            <div class="left-view">
+            <CaseListView></CaseListView>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import SuiteListView from './SuiteListView'
+    import CaseListView from './case/CaseListView'
+    export default {
+        components:{
+            SuiteListView,
+            CaseListView
+        }
+    }
+</script>
